c248202ee2c3ae2bfe67b39929649fe74c42aa9e
JT <jstorbit@gmail.com>
2025-11-20 17:47:31 -0700

    Add rendor elements and call the function

---
+++ index.js
@@ -0,0 +1,17 @@
+const rendor = () => {
+  const $app = document.querySelector(`#app`);
+  $app.innerHTML = `
+  <h1>Party Planner</h1>
+  <main>
+    <section>
+      <h2>Upcoming Parties</h2>
+      <PartyList></PartyList>
+    </section>
+    <section>
+      <h2>Party Details</h2>
+      <PartyDetails></PartyDetails>
+    </section>
+  </main>`;
+}
+
+rendor();
